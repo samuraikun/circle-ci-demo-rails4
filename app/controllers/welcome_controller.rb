9546698d08122e01464cd681a2df8611e71ddb04
samuraikun <tazitaziawawa@gmail.com>
2018-03-01 19:34:27 +0900

    add User model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @user = User.find_or_create_by(name: 'Tom', age: 24, sex: 'male')
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180301103029) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.integer  "age",        limit: 4
+    t.string   "sex",        limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
