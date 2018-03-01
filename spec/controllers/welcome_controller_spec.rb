e228cc1406c72b749032e6bb56a34b879687a6eb
samuraikun <tazitaziawawa@gmail.com>
2018-03-01 17:09:24 +0900

    hoge

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      # expect(response).to have_http_status(:success)
+      expect(response.status).to eq(200)
     end
   end
 end
